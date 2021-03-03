046f07bd7dee5885f98892e2904bd555b1b3c5f7
Kauan Vitorino <kautec@outlook.com.br>
2021-03-03 19:06:44 -0300

    Desafio de JS DIO

--- BootcampDIO/js/main.js
+++ BootcampDIO/js/main.js
@@ -1,5 +1,98 @@
 
-//Manipulando elementos da página
+let valor = prompt();
+let valorAux = 0;
+var nota100 = 0;
+var nota50 = 0;
+var nota20 = 0;
+var nota10 = 0;
+var nota5 = 0;
+var nota2 = 0;
+var nota1 = 0;
+
+valorAux = valor;
+
+if(valorAux >= 100)
+{
+  while(valorAux >= 100)
+  {
+    nota100 ++;   
+    valorAux = valorAux - 100; 
+  }
+  
+}
+
+if(valorAux >= 50)
+{ 
+  while(valorAux >= 50)
+  {
+    nota50 ++;    
+    valorAux = valorAux - 50 ;
+  }
+  
+}
+
+if(valorAux >= 20)
+{
+  while(valorAux >= 20)
+  {
+    nota20 ++;   
+    valorAux = valorAux - 20 ; 
+  }
+  
+}
+
+if(valorAux >= 10)
+{
+  while(valorAux >= 10)
+  {
+    nota10 ++;  
+    valorAux = valorAux - 10 ;  
+  }
+  
+}
+
+if(valorAux >= 5)
+{
+  while(valorAux >= 5)
+  {
+	  nota5 ++;	  
+      valorAux = valorAux - 5 ;
+  }
+ 
+}
+
+if(valorAux >= 2)
+{
+  while(valorAux >= 2)
+  {
+	  nota2 ++;	  
+      valorAux = valorAux - 2;
+  }
+  
+}
+
+nota1 = valorAux;
+
+console.log(valor);
+console.log(nota100 + " nota(s) de R$ 100,00");
+console.log(nota50 + " nota(s) de R$ 50,00");
+console.log(nota20 + " nota(s) de R$ 20,00");
+console.log(nota10 + " nota(s) de R$ 10,00");
+console.log(nota5 + " nota(s) de R$ 5,00");
+console.log(nota2 + " nota(s) de R$ 2,00");
+console.log(nota1 + " nota(s) de R$ 1,00");
+
+
+/* 5/5
+let distancia = parseInt(gets());
+let combustivel = parseFloat(gets());
+
+let valor = distancia / combustivel;
+
+console.log(valor.toFixed(3) + " km/l");
+ */
+
+/*//Manipulando elementos da página
 function botao()
 {
     document.getElementById("click").innerHTML = "<b>Obrigado por clicar</b>";
@@ -11,7 +104,7 @@ function redireciona()
     window.open("https://www.linkedin.com/");//nova aba
    // window.location.href = "https://www.linkedin.com/"; //mesma aba
 }
-
+*/
 /*
 function trocar()
 {
@@ -22,7 +115,7 @@ function volta()
 {
     document.getElementById("trocar").innerHTML = "Passe o mouse aqui";
 }
-*/
+*//*
 //Recebendo parametro
 function trocar(elemento)
 {
@@ -41,7 +134,7 @@ function load()
 function funcaoChange(elemento)
 {
     console.log(elemento.value);
-}
+}*/
 
 
 
